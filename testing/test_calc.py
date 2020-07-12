0f3fe8c33d71f6338c9958f0512637721bcfcafb
summer <2361157192@qq.com>
2020-07-13 07:45:55 +0800

    测试开发学习 第一次作业修改提交

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -2,6 +2,11 @@ from pythoncode.calc import Calculator
 import pytest
 
 
+def get_data():
+    with open("../datas/calc.yml") as f:
+        datas = f.read()
+
+
 class TestCalc:
 
     def setup_method(self):
@@ -16,7 +21,7 @@ class TestCalc:
         (0.5, 3, 3.5)
     ])
     @pytest.mark.add
-    def test_add(self, a, b, result, open):
+    def test_add(self, a, b, result):
         # cal = Calculator()
         assert self.cal.add(a, b) == result
 
@@ -30,7 +35,7 @@ class TestCalc:
         (0, 1, 0)
     ])
     @pytest.mark.div
-    def test_div(self, a, b, result, open):
+    def test_div(self, a, b, result):
         cal = Calculator()
         assert self.cal.div(a, b) == result
 
@@ -44,7 +49,7 @@ class TestCalc:
         (0, 1, 0)
     ])
     @pytest.mark.sub
-    def test_sub(self, a, b, result, open):
+    def test_sub(self, a, b, result):
         # cal = Calculator()
         assert self.cal.sub(a, b) == result
 
@@ -58,5 +63,5 @@ class TestCalc:
         (0, 1, 0)
     ])
     @pytest.mark.mul
-    def test_mul(self, a, b, result, open):
+    def test_mul(self, a, b, result):
         assert self.cal.mul(a, b) == result
