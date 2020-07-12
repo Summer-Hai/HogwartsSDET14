25792ab766ffbb17feeae54abe9a8c449f4f97ab
summer <2361157192@qq.com>
2020-07-13 07:59:06 +0800

    测试开发学习 第一次作业修改提交1

---
+++ testing/check_demo.py
@@ -0,0 +1,18 @@
+'''
+ -*- coding: utf-8 -*-
+ @Time    : 2020/7/12 15:25
+ @Author  : Summer
+ @Email   : 2361157192@qq.com
+ @File    : check_demo.py
+ 
+ '''
+
+
+def check_demo():
+    print("111")
+
+
+class CheckDemo():
+
+    def aaa_demo(self):
+        print("2222")
