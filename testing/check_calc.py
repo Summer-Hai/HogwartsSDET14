df5f02f5e2d56b161baf83c7f5e010f64c644d3c
summer <2361157192@qq.com>
2020-07-13 22:59:51 +0800

    测试开发学习 第2次作业修改提交1

---
+++ testing/check_calc.py
@@ -0,0 +1,51 @@
+'''
+ -*- coding: utf-8 -*-
+ @Time    : 2020/7/13 20:52
+ @Author  : Summer
+ @Email   : 2361157192@qq.com
+ @File    : check_calc.py
+ 
+ '''
+import yaml, pytest
+from pythoncode.calc import Calculator
+
+with open("../datas/calc1.yml") as f:
+    datas = yaml.safe_load(f)
+
+    adddatas = datas['add'].values()
+    addids = datas['add'].keys()
+
+    subdatas = datas['sub'].values()
+    subids = datas['sub'].keys()
+
+    muldatas = datas['mul'].values()
+    mulids = datas['mul'].keys()
+
+    divdatas = datas['div'].values()
+    divids = datas['div'].keys()
+
+
+class CheckCalc():
+
+    def setup(self):
+        self.cal = Calculator()
+
+    @pytest.mark.parametrize("a,b,result", adddatas, ids=addids)
+    def check_add(self, a, b, result):
+        print(f"\n计算数据：a={a},b={b},result={result}")
+        assert self.cal.add(a, b) == result
+
+    @pytest.mark.parametrize("a,b,result", divdatas, ids=divids)
+    def check_div(self, a, b, result):
+        print(f"\n计算数据：a={a},b={b},result={result}")
+        assert self.cal.div(a, b) == result
+
+    @pytest.mark.parametrize("a,b,result", subdatas, ids=subids)
+    def check_sub(self, a, b, result):
+        print(f"\n计算数据：a={a},b={b},result={result}")
+        assert self.cal.sub(a, b) == result
+
+    @pytest.mark.parametrize("a,b,result", muldatas, ids=mulids)
+    def check_mul(self, a, b, result):
+        print(f"\n计算数据：a={a},b={b},result={result}")
+        assert self.cal.mul(a, b) == result
--- testing/test_calc.py
+++ testing/test_calc.py
@@ -1,67 +1,38 @@
-from pythoncode.calc import Calculator
 import pytest
+import yaml, sys, os
+from pythoncode.calc import Calculator
+
+sys.path.append(os.path.abspath('../..'))
 
 
-def get_data():
-    with open("../datas/calc.yml") as f:
-        datas = f.read()
-
-
-class TestCalc:
-
-    def setup_method(self):
+class TestCalc1():
+    def setup(self):
         self.cal = Calculator()
-        print("setup_method")
 
-    @pytest.mark.parametrize(("a", "b", "result"), [
-        (1, 2, 3),
-        (-1, 2, 1),
-        (-1, -2, 3),
-        (0, 0, 0),
-        (0.5, 3, 3.5)
-    ])
     @pytest.mark.add
+    @pytest.mark.parametrize(('a', 'b', 'result'), yaml.safe_load(open(r'../datas/calc.yml'))['add'])
     def test_add(self, a, b, result):
-        # cal = Calculator()
+        print(f"计算数据：  a={a}, b={b}, result={result}")
         assert self.cal.add(a, b) == result
 
-    @pytest.mark.parametrize(("a", "b", "result"), [
-        (1, 2, 3),
-        (-1, 2, 1),
-        (-1, -2, 3),
-        (0, 0, 0),
-        (0.5, 3, 3.5),
-        (1, 0, 0),
-        (0, 1, 0)
-    ])
     @pytest.mark.div
+    @pytest.mark.parametrize(('a', 'b', 'result'), yaml.safe_load(open(r'../datas/calc.yml'))['div'])
     def test_div(self, a, b, result):
-        cal = Calculator()
+        print(f"计算数据：  a={a}, b={b}, result={result}")
         assert self.cal.div(a, b) == result
 
-    @pytest.mark.parametrize(("a", "b", "result"), [
-        (1, 2, 3),
-        (-1, 2, 1),
-        (-1, -2, 3),
-        (0, 0, 0),
-        (0.5, 3, 3.5),
-        (1, 0, 0),
-        (0, 1, 0)
-    ])
+    @pytest.mark.mul
+    @pytest.mark.parametrize(('a', 'b', 'result'), yaml.safe_load(open(r'../datas/calc.yml'))['mul'])
+    def test_mul(self, a, b, result):
+        print(f"计算数据：  a={a}, b={b}, result={result}")
+        assert self.cal.mul(a, b) == result
+
     @pytest.mark.sub
+    @pytest.mark.parametrize(('a', 'b', 'result'), yaml.safe_load(open(r'../datas/calc.yml'))['sub'])
     def test_sub(self, a, b, result):
-        # cal = Calculator()
+        print(f"计算数据： a={a}, b={b}, result={result}")
         assert self.cal.sub(a, b) == result
 
-    @pytest.mark.parametrize(("a", "b", "result"), [
-        (1, 2, 3),
-        (-1, 2, 1),
-        (-1, -2, 3),
-        (0, 0, 0),
-        (0.5, 3, 3.5),
-        (1, 0, 0),
-        (0, 1, 0)
-    ])
-    @pytest.mark.mul
-    def test_mul(self, a, b, result):
-        assert self.cal.mul(a, b) == result
+
+if __name__ == '__main__':
+    pytest.main(['check_calc.py', '-v', '-s'])
